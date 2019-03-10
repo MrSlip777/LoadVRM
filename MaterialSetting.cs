b2483c17547fe54c12f3151ef56b15d7cfab4644
MrSlip777 <mrslipgamecreate01@gmail.com>
2019-03-10 21:34:10 +0900

    create
    Materialの設定データを保存するスクリプト
    (ScriptableObject)

---
+++ MaterialSetting.cs
@@ -0,0 +1,10 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+[CreateAssetMenu( menuName = "VRM/MaterialSetting", fileName = "MaterialSetting" )]
+public class MaterialSetting: ScriptableObject
+{
+    public string targetName;
+    public Material[] mat = new Material[10];
+}
