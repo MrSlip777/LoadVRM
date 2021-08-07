62dbde1127ba729f8711545caa016cdb03398ea3
slip <mrslipgamecreate01@gmail.com>
2021-08-07 23:37:29 +0900

    draft
    ReflectBlendShapesのスクリプトを追加

---
+++ ReflectBlendShapes/ReflectBlendShapes.cs
@@ -0,0 +1,101 @@
+﻿/*
+ * 対象モデルのBlendShapeに他のBlendShape設定を反映させるスクリプト
+ *
+ * (C)2021 slip
+ * This software is released under the MIT License.
+ * http://opensource.org/licenses/mit-license.php
+ * [Twitter]: https://twitter.com/kjmch2s/
+ *
+ * 利用規約：
+ *  作者に無断で改変、再配布が可能で、利用形態（商用、18禁利用等）
+ *  についても制限はありません。
+ *  このスクリプトはもうあなたのものです。
+ * 
+ */
+#if UNITY_EDITOR
+
+
+using UnityEditor;
+using UnityEngine;
+using System;
+using System.IO;
+
+namespace VRM
+{
+
+    public class ReplaceFile : ScriptableWizard
+    {
+
+        //対象になるブレンドシェイプ
+        public BlendShapeAvatar sourceBlendShape;
+        public BlendShapeAvatar destinationBlendShape;
+
+        readonly string RefName = "BlendShape.asset";
+
+        public static void CreateWizard()
+        {
+            var wiz = ScriptableWizard.DisplayWizard<ReplaceFile>(
+                "ApplyBlendShape", "Apply");
+        }
+
+        void OnWizardCreate()
+        {
+            string projectPath = null;
+            string srcFullPath = null;
+            string srcDir = null;
+            string[] srcFiles = null;
+
+            string dstFullPath = null;
+            string dstDir = null;
+            string[] dstFiles = null;
+
+            projectPath = Application.dataPath.Replace("Assets","");
+            
+            srcFullPath = projectPath + AssetDatabase.GetAssetPath(sourceBlendShape);
+            srcDir = System.IO.Path.GetDirectoryName(srcFullPath);
+            srcFiles = System.IO.Directory.GetFiles(srcDir,"*");
+
+            dstFullPath = projectPath + AssetDatabase.GetAssetPath(destinationBlendShape);
+            dstDir = System.IO.Path.GetDirectoryName(dstFullPath);
+            dstFiles = System.IO.Directory.GetFiles(dstDir,"*");
+
+            //Unity内で使用できるようにする
+            srcDir = srcDir.Replace("\\","/");
+            dstDir = dstDir.Replace("\\","/");
+
+            foreach (var item in srcFiles)
+            {
+                string srcPath = null;
+                string dstPath = null;
+                string targetFile = null;
+
+                targetFile = System.IO.Path.GetFileName(item);
+                
+                //BlendShape.assetでなければ、コピーする（各ブレンドシェイプをコピーする）
+                if(targetFile != RefName && targetFile.Contains(".meta") == false){
+
+                    srcPath = srcDir + "/" + targetFile;
+                    dstPath = dstDir + "/" + targetFile;
+                
+                    File.Copy(srcPath, dstPath,true);
+
+                }
+            }
+
+            AssetDatabase.Refresh();
+        }
+    }
+
+    public static class ReplaceFileMenu
+    {
+        const string ADD_OPTIONOBJECT_KEY = VRMVersion.VRM_VERSION + "/ReplaceBlendShapes";
+
+        [MenuItem(ADD_OPTIONOBJECT_KEY)]
+        private static void Menu()
+        {
+            ReplaceFile.CreateWizard();
+        }
+    }
+}
+
+#endif
